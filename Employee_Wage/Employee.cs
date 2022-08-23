321b992f736177c3a9bccd7b770b07519e2cdb13
sayalipagar <pagarsayali565@gmail.com>
2022-08-23 11:57:19 +0530

    [sayali]Add:EmployeeAttendance

--- Employee_Wage/Employee.cs
+++ Employee_Wage/Employee.cs
@@ -8,5 +8,18 @@ namespace Employee_Wage
 {
     internal class Employee
     {
+        public void Attendance()
+        {
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 0)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+
+
+
     }
 }
